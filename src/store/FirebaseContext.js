8fce34303e0a497e5358ec357cff994bd0fd7915
femi <femi1602@gmail.com>
2021-06-22 17:24:41 +0530

    Deploy

--- src/store/FirebaseContext.js
+++ src/store/FirebaseContext.js
@@ -1,4 +1,15 @@
-import {createContext} from 'react'
+import {createContext, useState} from 'react'
 
 export const FirebaseContext = createContext(null)
 
+export const AuthContext=createContext(null);
+
+
+export default function Context({children}){
+    const[user,state]=useState(null)
+    return (
+        <AuthContext.Provider>
+
+        </AuthContext.Provider>
+    )
+}
