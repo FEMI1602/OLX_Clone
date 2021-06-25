da147818adacf9f1292121061496a87f8cfedcfd
femi <femi1602@gmail.com>
2021-06-25 14:18:25 +0530

    Updated

--- src/store/FirebaseContext.js
+++ src/store/FirebaseContext.js
@@ -6,10 +6,10 @@ export const AuthContext=createContext(null);
 
 
 export default function Context({children}){
-    const[user,state]=useState(null)
+    const[user,setUser]=useState(null)
     return (
-        <AuthContext.Provider>
-
+        <AuthContext.Provider value={{user}}>
+                {children}
         </AuthContext.Provider>
     )
 }
